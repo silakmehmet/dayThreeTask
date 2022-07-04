dd4f7472bfab5388d8ff50de7a01b8e8b70f02ca
silakmehmet <82677215+silakmehmet@users.noreply.github.com>
2022-07-04 11:57:24 +0300

    Delete UserManager.java

--- UserManager.java
+++
@@ -1,8 +0,0 @@
-package dayThreeTask;
-
-public class UserManager {
-	public void addUser(User user) {
-		System.out.println(user.getFirstName()+" "+ user.getLastName()+ " isimli kullanýcý oluþturuldu.");
-		
-		}
-	}
