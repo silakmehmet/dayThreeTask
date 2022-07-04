16f37e01d7bc3c0754bcadae4d8beebcbc64232a
silakmehmet <82677215+silakmehmet@users.noreply.github.com>
2022-07-04 11:56:59 +0300

    Delete StudentManager.java

--- StudentManager.java
+++
@@ -1,8 +0,0 @@
-package dayThreeTask;
-
-public class StudentManager {
-	public void addUser(Student student) {
-		System.out.println(student.getFirstName()+" "+student.getLastName()+ " isimli kullanıcı oluşturuldu.");
-	}
-
-}
